c3990ddc9f3ff773d7b63a54ea88afcae824fb15
Catherine Hu <catherine.gobears@berkeley.edu>
2018-12-02 17:01:11 -0800

    test stdIO success

--- hw1/hw1_util/unitTest.rb
+++ hw1/hw1_util/unitTest.rb
@@ -4,6 +4,7 @@ require 'test/unit'
 
 require_relative '../old_roman'
 require_relative '../square'
+require_relative '../bigger_fav'
 
 class HW1_Test < Test::Unit::TestCase
     def test_old_roman
@@ -13,5 +14,20 @@ class HW1_Test < Test::Unit::TestCase
 
     def test_square
         assert(square(3) == 9)
+    end
+
+    def test_bigger_fav
+        stdin = $stdin
+        stdout = $stdout
+        input = StringIO.new("11")
+        output = StringIO.new
+        $stdin = input
+        $stdout = output
+
+        bigger_favorite_number
+
+        $stdin = stdin
+        $stdout = stdout
+        assert(output.string == "Hey, what\'s your favorite number?\nThe bigger and better favorite number is 12\n")
     end
 end
